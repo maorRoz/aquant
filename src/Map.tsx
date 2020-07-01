1e29f4519a53fedd5a69b7df258560a5914a3f17
Maor Rozevich <andorys@post.bgu.ac.il>
2020-07-01 15:47:09 +0300

    brought regularPolygons porp back

--- src/Map.tsx
+++ src/Map.tsx
@@ -11,12 +11,12 @@ export const Map = ({ pushPins }: MapProps) => (
   <ReactBingmaps
     bingmapKey='Aq1y7_VVtmr_2GiYdgS4uwFH6qwigTxk2y0fv43_bGpqG-nRkJq4Us6KOebmRmmS'
     pushPins={pushPins}
-    /*     regularPolygons={[
+    regularPolygons={[
       {
         rings: pushPins.map((pin) => pin.location),
         points: pushPins.length,
         option: { fillColor: 'green', strokeThickness: 2 }
       }
-    ]} */
+    ]}
   />
 );
